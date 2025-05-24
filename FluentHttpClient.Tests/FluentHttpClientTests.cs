728a10e901feae0fc16d912567f7d083014c9f93
Kasper Mark Søndergaard <kaspermarks@gmail.com>
2025-05-24 17:20:24 +0200

    Testing CI with github actions

--- FluentHttpClient.Tests/FluentHttpClientTests.cs
+++ FluentHttpClient.Tests/FluentHttpClientTests.cs
@@ -21,4 +21,22 @@ public class FluentHttpClientTests
         var content = await response.Content.ReadAsStringAsync();
         content.Should().Contain("\"id\": 1");
     }
+
+    [Fact]
+    public async Task Get_ToPublicApi_ShouldReturnSuccessStatusCodeNoBody()
+    {
+        // Arrange
+        var client = new FluentClient(new HttpClient());
+
+        // Act
+        var response = await client
+            .Get()
+            .Url("https://jsonplaceholder.typicode.com/posts/1")
+            .ExecuteAsync();
+
+        // Assert
+        response.IsSuccessStatusCode.Should().BeTrue();
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Contain("\"id\": 1");
+    }
 }
