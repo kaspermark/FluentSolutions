f30c72c1718bb32d813829d7930c9cfaafeb80d6
Jonas Mertz <jonasmertz@gmail.com>
2025-05-25 20:07:22 +0200

    Add query Parameter support (#13)
    * remove lies
    
    * add queryParameter support for fluent client
    
    * formatting

--- FluentHttpClient.Tests/GeneralFluentHttpClientTests.cs
+++ FluentHttpClient.Tests/GeneralFluentHttpClientTests.cs
@@ -280,4 +280,90 @@ public class GeneralFluentHttpClientTests
 
         act.Should().Throw<ArgumentException>();
     }
+
+    [Theory]
+    [InlineData("GET")]
+    [InlineData("POST")]
+    [InlineData("PUT")]
+    [InlineData("PATCH")]
+    [InlineData("DELETE")]
+    public void AddQueryParameters_WithEmptyName_ShouldThrowArgumentException(string methodName)
+    {
+        var clientBuilder = methodName switch
+        {
+            "GET" => Client.Get(),
+            "POST" => Client.Post(),
+            "PUT" => Client.Put(),
+            "PATCH" => Client.Patch(),
+            "DELETE" => Client.Delete(),
+            _ => throw new NotSupportedException()
+        };
+
+        Action act = () => clientBuilder
+            .UseHttps("httpbin.org/anything").AddQueryParam("", "value");
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData("GET", "Params1", "Value")]
+    [InlineData("POST", "Params1", "Value")]
+    [InlineData("PUT", "Params1", "Value")]
+    [InlineData("PATCH", "Params1", "Value")]
+    [InlineData("DELETE", "Params1", "Value")]
+
+    public async Task AddQueryParameters_ShouldIncludeQueryParametersInURI(string method, string name, string value)
+    {
+        var httpMethod = new HttpMethod(method);
+
+        var clientBuilder = method switch
+        {
+            "GET" => Client.Get(),
+            "POST" => Client.Post(),
+            "PUT" => Client.Put(),
+            "PATCH" => Client.Patch(),
+            "DELETE" => Client.Delete(),
+            _ => throw new NotSupportedException()
+        };
+
+        clientBuilder
+            .UseHttps("httpbin.org/anything")
+            .AddQueryParam(name, value);
+
+        var response = await clientBuilder.ExecuteAsync();
+        response.RequestMessage.Should().NotBeNull();
+        response.RequestMessage.RequestUri.Should().NotBeNull();
+        response.RequestMessage.RequestUri.Query.Should().Contain(name + "=" + value);
+    }
+
+    [Theory]
+    [InlineData("GET")]
+    [InlineData("POST")]
+    [InlineData("PUT")]
+    [InlineData("PATCH")]
+    [InlineData("DELETE")]
+    public async Task AddQueryParameters_ShouldIncludeMultipleQueryParametersInURI(string methodName)
+    {
+        var clientBuilder = methodName switch
+        {
+            "GET" => Client.Get(),
+            "POST" => Client.Post(),
+            "PUT" => Client.Put(),
+            "PATCH" => Client.Patch(),
+            "DELETE" => Client.Delete(),
+            _ => throw new NotSupportedException()
+        };
+
+        clientBuilder
+            .UseHttps("httpbin.org/anything")
+            .AddQueryParam("Param1", "value")
+            .AddQueryParam("Param2", "value");
+
+        var response = await clientBuilder.ExecuteAsync();
+        response.RequestMessage.Should().NotBeNull();
+        response.RequestMessage.RequestUri.Should().NotBeNull();
+        response.RequestMessage.RequestUri.Query.Should().Contain("Param1=value");
+        response.RequestMessage.RequestUri.Query.Should().Contain("Param2=value");
+
+    }
 }
